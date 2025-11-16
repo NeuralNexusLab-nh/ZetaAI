9c75e18a31eb9f8be09b12d353b1366b4a6f853f
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-11-16 17:52:30 +0800

    Update zeta.js

--- zeta.js
+++ zeta.js
@@ -1,13 +1,4 @@
 function zeta (q) {
-    function processHTML(html) {
-      html = html.replace(/<head[\s\S]*?>[\s\S]*?<\/head>/gi, '');
-      html = html.replace(/<style[\s\S]*?>[\s\S]*?<\/style>/gi, '');
-      html = html.replace(/<a\s+[^>]*>([\s\S]*?)<\/a>/gi,(_, text) => text.replace(/<[^>]*>/g, '').trim());
-      html = html.replace(/<(p|h[1-6]|label)[^>]*>([\s\S]*?)<\/\1>/gi,(_, tag, content) => content.replace(/<[^>]*>/g, '').trim() + "\n");
-      html = html.replace(/<[^>]*>/g, '');
-      return html.replace(/\n{2,}/g, '\n').trim();
-  }
-
   const db = [
     `Who am I? I'm a model trained by NeuralNexusLab and developed by NeuralNexusLab. I can use article to answer you or computation math.`,
     `Who are you? I don't know who are you but I know you are my best friend! I can explain what is AI to you, or computation math.`,
@@ -62,20 +53,6 @@ function zeta (q) {
   keyword = keyword.replace(/[^a-zA-Z\s]/g, '');
   var catc = "";
   var isntAdd = true
-
-  if (q.toLowerCase().includes("web") || q.toLowerCase().includes("net")) {
-    var plhd = q.toLowerCase().split(" ");
-    var url;
-    for (let i = 0; i < raw.length; i++) {
-      if (plhd[i].startsWith("http://") || plhd[i].startsWith("https://")) {
-        url = plhd[i];
-      }
-    }
-    isntAdd = false;
-    fetch(`https://hackdev.nethacker.cloud/fetch?url=${url}`).then(res => res.text()).then((data) => {
-      catc += processHTML(data);
-    });
-  }
 
   for (let i = 0; i < db.length; i++) {
     if (db[i].toLowerCase().includes(keyword.toLowerCase()) && isntAdd === true) {
